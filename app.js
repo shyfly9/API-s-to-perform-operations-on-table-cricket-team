21f350dc4d72769a8067f5978de71918039bad13
shyam <yash.shyam007@gmail.com>
2023-02-03 15:44:45 +0000

    API's to perform operations on table cricket_team

--- app.js
+++ app.js
@@ -58,15 +58,15 @@ app.post("/players/", async (request, response) => {
   const { playerName, jerseyNumber, role } = playerDetails;
   const addedPlayerQuery = `
     INSERT INTO
-      cricket_team (playerName,jerseyNumber,role)
+      cricket_team (player_name,jersey_number,role)
     VALUES
       (
         '${playerName}',
-         '${jerseyNumber}',
-         '${role}',);`;
+         ${jerseyNumber},
+         '${role}');`;
   await db.run(addedPlayerQuery);
 
-  response.send("Player added to team");
+  response.send("Player Added to Team");
 });
 
 //get one player
@@ -92,12 +92,25 @@ app.put("/players/:playerId/", async (request, response) => {
     UPDATE
       cricket_team
     SET
-     playerName=${playerName}',
-     jerseyNumber=${jerseyNumber}',
-     role=${role}'
+     player_name='${playerName}',
+     jersey_number=${jerseyNumber},
+     role='${role}'
      
     WHERE
       player_id = ${playerId};`;
   await db.run(updatePlayerQuery);
   response.send("Player Details Updated");
 });
+
+//delete
+app.delete("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const deletePlayerQuery = `
+    DELETE FROM
+      cricket_team
+    WHERE
+      player_id = ${playerId};`;
+  await db.run(deletePlayerQuery);
+  response.send("Player Removed");
+});
+module.exports = app;
